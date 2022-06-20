2fc1b41fc06e7e6622314ce9439cd6d7bea9381b
Tim Hårek Andreassen <tim@harek.no>
2022-06-20 10:08:42 +0200

    fix: Move all theme logic to single JS-file
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,4 +1,31 @@
-window.onload(detectColorScheme())
+// identify the toggle switch HTML element
+const toggleSwitch = document.querySelector("#checkbox_theme")
+
+// function that changes the theme, and sets a localStorage variable to track the theme between page loads
+function switchTheme(e) {
+  if (e.target.checked) {
+    localStorage.setItem("theme", "dark")
+    document.documentElement.setAttribute("data-theme", "dark")
+    toggleSwitch.checked = true
+  } else {
+    localStorage.setItem("theme", "light")
+    document.documentElement.setAttribute("data-theme", "light")
+    toggleSwitch.checked = false
+  }
+}
+
+// listener for changing themes
+toggleSwitch.addEventListener("change", switchTheme, false)
+
+function isDarkTheme() {
+  // pre-check the dark-theme checkbox if dark-theme is set
+  if (document.documentElement.getAttribute("data-theme") == "dark") {
+    toggleSwitch.checked = true
+  }
+}
+
+window.onload = detectColorScheme()
+window.onload = isDarkTheme()
 
 // determines if the user has a set theme
 function detectColorScheme() {
