00bd1e44fa3d9e05be173a3051405eab7d35b83e
Tim Hårek Andreassen <tim@harek.no>
2023-02-27 17:32:23 +0100

    fix: Theme switcher jump on page load
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,51 +1,27 @@
 // identify the toggle switch HTML element
-const toggleSwitch = document.querySelector("#checkbox_theme")
+const toggleSwitch = document.querySelector("#checkbox_theme");
 
 // function that changes the theme, and sets a localStorage variable to track the theme between page loads
 function switchTheme(e) {
   if (e.target.checked) {
-    localStorage.setItem("theme", "dark")
-    document.documentElement.setAttribute("data-theme", "dark")
-    toggleSwitch.checked = true
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+    toggleSwitch.checked = true;
   } else {
-    localStorage.setItem("theme", "light")
-    document.documentElement.setAttribute("data-theme", "light")
-    toggleSwitch.checked = false
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+    toggleSwitch.checked = false;
   }
 }
 
 // listener for changing themes
-toggleSwitch.addEventListener("change", switchTheme, false)
+toggleSwitch.addEventListener("change", switchTheme, false);
 
 function isDarkTheme() {
   // pre-check the dark-theme checkbox if dark-theme is set
   if (document.documentElement.getAttribute("data-theme") == "dark") {
-    toggleSwitch.checked = true
+    toggleSwitch.checked = true;
   }
 }
 
-window.onload = detectColorScheme()
-window.onload = isDarkTheme()
-
-// determines if the user has a set theme
-function detectColorScheme() {
-  let theme = "light" //default to light
-
-  // local storage is used to override OS theme settings
-  if (localStorage.getItem("theme")) {
-    if (localStorage.getItem("theme") == "dark") {
-      theme = "dark"
-    }
-  } else if (!window.matchMedia) {
-    // matchMedia method not supported
-    return false
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    // OS theme setting detected as dark
-    theme = "dark"
-  }
-
-  // dark theme preferred, set document with a `data-theme` attribute
-  if (theme == "dark") {
-    document.documentElement.setAttribute("data-theme", "dark")
-  }
-}
+window.onload = isDarkTheme();
